45c46625e7748f4cd9f6a8083b36c4bb554b686f
Volodymyr Zabila <wzabila1995@gmail.com>
2024-02-02 22:16:06 +0200

    Update route in TransactionController

--- WalletService/WalletService.Presentation/Controllers/TransactionController.cs
+++ WalletService/WalletService.Presentation/Controllers/TransactionController.cs
@@ -7,7 +7,7 @@ using Shared.DataTransferObjects;
 
 namespace WalletService.Presentation.Controllers;
 
-[Route("api/{accountId:guid}/[controller]")]
+[Route("api/accounts/{accountId:guid}/[controller]")]
 [ApiController]
 public class TransactionsController(ISender sender) : ControllerBase
 {
