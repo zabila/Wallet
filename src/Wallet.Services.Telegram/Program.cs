818baac6425594f4b656f88e7b234e20727bb38c
Volodymyr Zabila <wzabila1995@gmail.com>
2024-02-17 18:02:38 +0200

    Refactor Telegram service setup into separate methods
    The setup for the Telegram service in the main Program.cs file was refactored into new methods in a new class 'ServiceExtensions.cs'. By encapsulating these configurations into well-named methods - ConfigureLoggerService and ConfigureTelegramService, the code has become more modular, easier to read, and simpler to manage. Any updates or adjustments can be made within these methods without touching the main program's setup code.

--- src/Wallet.Services.Telegram/Program.cs
+++ src/Wallet.Services.Telegram/Program.cs
@@ -1,42 +1,10 @@
-using Telegram.Bot;
-using Wallet.Services.Telegram.AsyncDataServices;
-using Wallet.Services.Telegram.Contracts;
-using Wallet.Services.Telegram.Handlers;
-using Wallet.Services.Telegram.Services;
-using Wallet.Services.Telegram.SyncDataServices.Http;
-using Wallet.Services.Telegram.WalletStates;
-using Wallet.Services.Telegram.WalletStates.Incoming;
-using Wallet.Services.Telegram.WalletStates.Outcoming;
+using Wallet.Services.Telegram.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddSingleton<ILoggerManager, LoggerManager>();
-
-//Get token from env variable
-var telegramBotToken = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN");
-if (string.IsNullOrEmpty(telegramBotToken))
-{
-    throw new ArgumentNullException(nameof(telegramBotToken), "Telegram bot token is not set");
-}
-
-builder.Services.AddHttpClient("telegram_bot_client")
-    .AddTypedClient<ITelegramBotClient>((httpClient, _) =>
-    {
-        TelegramBotClientOptions options = new(telegramBotToken);
-        return new TelegramBotClient(options, httpClient);
-    });
-
-builder.Services.AddHttpClient<IWalletDataClient, HttpWalletDataClient>();
-builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
-
-builder.Services.AddScoped<IWalletContext, WalletContext>();
-builder.Services.AddScoped<IWalletState, IncomingState>();
-builder.Services.AddScoped<IWalletState, OutcomingState>();
-
-builder.Services.AddScoped<UpdateHandler>();
-builder.Services.AddScoped<ReceiverService>();
-builder.Services.AddHostedService<PollingService>();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureTelegramService();
 
 var app = builder.Build();
 app.Run();
