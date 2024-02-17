80dddf6fd53633c515c49bb3b8bb969756742035
Volodymyr Zabila <wzabila1995@gmail.com>
2024-02-17 18:02:38 +0200

    Refactor Telegram service setup into separate methods
    The setup for the Telegram service in the main Program.cs file was refactored into new methods in a new class 'ServiceExtensions.cs'. By encapsulating these configurations into well-named methods - ConfigureLoggerService and ConfigureTelegramService, the code has become more modular, easier to read, and simpler to manage. Any updates or adjustments can be made within these methods without touching the main program's setup code.

---
+++ src/Wallet.Services.Telegram/Extensions/ServiceExtensions.cs
@@ -0,0 +1,48 @@
+using NLog;
+using Wallet.Services.Telegram.Contracts;
+using Wallet.Services.Telegram.Services;
+using Wallet.Services.Telegram.Handlers;
+using Wallet.Services.Telegram.SyncDataServices.Http;
+using Wallet.Services.Telegram.WalletStates;
+using Wallet.Services.Telegram.WalletStates.Incoming;
+using Wallet.Services.Telegram.WalletStates.Outcoming;
+using Telegram.Bot;
+using Wallet.Services.Telegram.AsyncDataServices;
+
+namespace Wallet.Services.Telegram.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureLoggerService(this IServiceCollection services)
+    {
+        LogManager.Setup().LoadConfigurationFromFile(Path.Combine(Directory.GetCurrentDirectory(), "nlog.config"));
+        services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+    
+    public static void ConfigureTelegramService(this IServiceCollection services)
+    {
+        var telegramBotToken = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN");
+        if (string.IsNullOrEmpty(telegramBotToken))
+        {
+            throw new ArgumentNullException(nameof(telegramBotToken), "Telegram bot token is not set");
+        }
+
+        services.AddHttpClient("telegram_bot_client")
+            .AddTypedClient<ITelegramBotClient>((httpClient, _) =>
+            {
+                TelegramBotClientOptions options = new(telegramBotToken);
+                return new TelegramBotClient(options, httpClient);
+            });
+        
+        services.AddHttpClient<IWalletDataClient, HttpWalletDataClient>();
+        services.AddSingleton<IMessageBusClient, MessageBusClient>();
+        
+        services.AddScoped<IWalletContext, WalletContext>();
+        services.AddScoped<IWalletState, IncomingState>();
+        services.AddScoped<IWalletState, OutcomingState>();
+        
+        services.AddScoped<UpdateHandler>();
+        services.AddScoped<ReceiverService>();
+        services.AddHostedService<PollingService>();
+    }
+}
