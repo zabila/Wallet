128ec3a66d3eaf0ca1836ccebee291b6e1bb2956
Volodymyr Zabila <wzabila1995@gmail.com>
2024-03-28 14:47:46 +0200

    Add dependencies and update launch configuration and app module

--- client_angular/src/app/app.component.ts
+++ client_angular/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'client_angular';
+export class AppComponent implements OnInit {
+    title = 'Wallet';
+    transactions: any;
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+        this.http
+            .get(
+                'http://localhost:5032/api/account/fe7d0297-946a-4a7e-8d03-64dee035babf/transactions'
+            )
+            .subscribe({
+                next: (response) => (this.transactions = response),
+                error: (error) => console.log(error),
+                complete: () => console.log('completed'),
+            });
+    }
 }
