bfd04c299451bf0aa152f09bffff6abcc9b43af1
Volodymyr Zabila <wzabila1995@gmail.com>
2024-04-07 18:20:16 +0300

    remove unused value

--- client_angular/src/app/_guards/auth.guard.ts
+++ client_angular/src/app/_guards/auth.guard.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { AuthenticationService } from '../_services/authentication.service';
-import { UserService } from './../_services/user.service';
 
 @Injectable({
     providedIn: 'root',
@@ -10,8 +9,7 @@ import { UserService } from './../_services/user.service';
 export class AuthGuard {
     constructor(
         private authenticationService: AuthenticationService,
-        private userService: UserService,
-        private router: Router
+        private router: Router,
     ) {}
 
     canActivate = () => {
@@ -27,7 +25,7 @@ export class AuthGuard {
                 console.log('Not authenticated, redirecting to login page');
                 this.router.navigate(['/login']);
                 return false;
-            })
+            }),
         );
     }
 }
