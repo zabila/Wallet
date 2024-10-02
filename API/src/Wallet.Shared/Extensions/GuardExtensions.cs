1aa898272d0472ef06da971f2388204e66e5d194
Volodymyr Zabila <wzabila1995@gmail.com>
2024-10-02 21:51:51 +0300

    Add EnsureExists method and refactor null checks

---
+++ API/src/Wallet.Shared/Extensions/GuardExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Wallet.Shared.Extensions;
+
+public static class GuardExtensions {
+    public static T EnsureExists<T>(this T? obj) where T : class? {
+        ArgumentNullException.ThrowIfNull(obj);
+        return obj;
+    }
+
+    public static string EnsureExists(this string? obj) {
+        ArgumentException.ThrowIfNullOrEmpty(obj);
+        return obj;
+    }
+}
